d0bf0d956d11bf0deb821df579cfb8511afd37e5
Tianyi <tianyi_ty@outlook.com>
2025-12-04 16:26:58 +0800

    led

---
+++ bsp/inc/switch.h
@@ -0,0 +1,9 @@
+#ifndef __SWITCH_H__
+#define __SWITCH_H__
+
+#include "stm32f4xx.h"
+
+void Switch_Lib_Init(void);
+void Switch_Reg_Init(void);
+
+#endif
---
+++ bsp/src/switch.c
@@ -0,0 +1,15 @@
+#include "switch.h"
+void Switch_Lib_Init(){
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Pin   = 0xFF;
+    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IN;
+    GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOF, &GPIO_InitStructure);
+}
+
+void Switch_Reg_Init(){
+    RCC->APB1ENR |= 1 << 5; 
+    GPIOF->MODER &= 0x00000000;
+    GPIOF->PUPDR = 0x55555555;
+}
