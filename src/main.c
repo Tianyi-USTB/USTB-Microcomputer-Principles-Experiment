ae753441d542a6ee5e937a3375d68a7b8de8746f
Tianyi <tianyi_ty@outlook.com>
2025-12-04 16:26:38 +0800

    led

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 
 #include "cmsis_gcc.h"
 #include "stm32f4xx.h"
+#include "led.h"
+#include "switch.h"
 
 void SystemClock_Config(void)
 {
@@ -40,22 +42,11 @@ int main(void)
     SystemClock_Config();
     SysTick_Config(SystemCoreClock / 1000); // 1ms tick
 
-    GPIO_InitTypeDef GPIO_InitStructure;
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
-
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
-
-    GPIO_ResetBits(GPIOE, GPIO_Pin_1);
-
-    GPIO_InitStructure.GPIO_Pin   = 0xFF;
-    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-
-   GPIO_Init(GPIOE, &GPIO_InitStructure);
+    LED_Lib_Init();
+    Switch_Lib_Init();
 
     while (1) {
-        GPIO_ToggleBits(GPIOA, 0xFF);
-        Delay(500);
+        GPIOE->ODR = (GPIOE->ODR & 0xFF << 8) | ~(GPIOF->IDR & 0xFF);
+        // Delay(500);
     }
 }
